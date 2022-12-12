9e0872b8daa18fac224c3b77cbc88b6c73c38c8f
Arya Shah <57292897+Arya-Shah@users.noreply.github.com>
2022-12-12 12:37:46 -0500

    Added sessions.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-const userRoutes = require("./users");
+const userRoutes = require("./user");
 const garageOwnerRoutes = require("./garageOwner");
 const loginRoutes = require("./login");
 
 const constructorMethod = app => {
-    app.use("/users", userRoutes);
+    app.use("/user", userRoutes);
     app.use("/garage", garageOwnerRoutes);
     app.use("/login", loginRoutes);
 
-    app.get('/', (req, res) => {
-        res.redirect('http://localhost:3000/login');
-    });
+    // app.get('/', (req, res) => {
+    //     res.redirect('http://localhost:3000/login');
+    // });
 
-    app.use("*", (req, res) => {
+    app.use('*', (req, res) => {
         res.sendStatus(404);
     });
 };
