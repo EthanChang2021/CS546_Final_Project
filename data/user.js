cfc73a589d1fbd26ba4e154e03e33465c78a0c04
shivamp2601 <115746101+shivamp2601@users.noreply.github.com>
2022-12-13 15:04:56 -0500

    Added getHistory Functionality.

--- data/user.js
+++ data/user.js
@@ -15,7 +15,7 @@ const createUser = async (
     const hash = await bcrypt.hash(password, saltRounds);
     console.log(hash);
 
-    const userCollection = await users();
+    const userCollection = await users();  
     let newUser = {
         username: username,
         password: hash,
@@ -50,6 +50,19 @@ const checkUser = async (username, password) => {
     }
 };
 
+const getHistory = async(id) =>{
+    try{
+        const userCollection = await users();
+        const userExist = await userCollection.findOne({ id: id_ });
+        if (!userExist) return { msg: "Either the username or password is invalid" }
+        return userExist.history;
+    
+
+    }catch(e){
+        throw(`History Error:${e}`)
+    }
+}
+
 const favoriteOfUser = async(id,favoriteGarage) => {
     try{
     let favorites_ = await getFavorites(id);
@@ -60,6 +73,7 @@ const favoriteOfUser = async(id,favoriteGarage) => {
     }
 }
 const getFavorites = async(id_) =>{
+    const userCollection = await users();  
     const userExist = await userCollection.findOne({ id: id_ });
     if (!userExist) return { msg: "Either the username or password is invalid" }
     return userExist.favorite
@@ -67,14 +81,22 @@ const getFavorites = async(id_) =>{
 }
 
 const setFavorite = async(_id,favorite_) =>{
+    try{
+    const userCollection = await users();  
     const userExist = await userCollection.findOne({ id: id_ });
     if (!userExist) return { msg: "Either the username or password is invalid" }
-    
-
+    const myquery = { id: _id};
+    const newFavorite = { $set: {favorite: favorite_} };
+    await userCollection.updateOne(myquery,newFavorite)
+    }catch(e){
+        console.log(`Error favorit:${e}`);
+    }
 
 }
+
 module.exports = {
     createUser,
     checkUser,
-    favoriteOfUser
+    favoriteOfUser,
+    getHistory
 };
